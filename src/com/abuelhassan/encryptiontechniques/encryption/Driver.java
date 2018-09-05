0efbdd2a6dc6c7fbbb69c3264070e3c78d04fc75
abuelhassan <>
2018-09-05 16:24:49 +0200

    First Init

---
+++ src/com/abuelhassan/encryptiontechniques/encryption/Driver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abuelhassan.encryptiontechniques.encryption;
+
+/**
+ *
+ * @author Abuelhassan
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Home ctw = new Home();
+        ctw.setSize(580, 463);
+        ctw.setLocation(400, 150);
+        ctw.setVisible(true);
+    }
+
+}
